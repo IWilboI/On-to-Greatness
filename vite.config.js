d091feba7257e362de59421f3fb6e57192facaae
William Callaway <167031800+IWilboI@users.noreply.github.com>
2024-10-25 15:41:43 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // Make sure this is pointing to the public folder
+  root: 'public', 
   build: {
     outDir: '../dist',
   },
