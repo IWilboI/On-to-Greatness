d41a4a4c5a644175b2e0077bee1a87798018be56
William Callaway <167031800+IWilboI@users.noreply.github.com>
2024-10-25 15:41:19 -0600

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Ensure this path is correct
+import App from './App'; 
 
 const rootElement = document.getElementById('root');
 
